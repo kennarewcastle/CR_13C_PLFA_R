2f20ed74c1f0b9ae61404b58bf2cd66245d2479a
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-04-18 11:21:30 -0400

    Updates to FAME_calculations.

--- FAME_calculations.R
+++ FAME_calculations.R
@@ -2,3 +2,8 @@
 # 18 April 2018
 # KER
 
+
+# Read in data frame that was created with append functions in FAME_data_prep.R
+data<-read.csv("MASTER_DAT_W_SOIL.csv") 
+data<-data[,2:9] # Get rid of extra numerical ID column
+
