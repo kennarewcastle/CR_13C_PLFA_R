1054a7d1ab4bb0faf6fd29fe5f15011f3ca5431e
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2021-12-09 16:57:53 -0500

    Corrected the d13C-PLFA figures.

--- Current_CostaRica_Graphs.R
+++ Current_CostaRica_Graphs.R
@@ -9,6 +9,8 @@ library(dplyr)
 library(ggplot2)
 library(gridExtra)
 library(reshape2)
+library(viridis)
+library(car)
 
 # delta13C-CO2 ------------------------------------------------------------
 
@@ -82,12 +84,19 @@ library(reshape2)
   data_trim<-data.frame("Sample_Name"=data[,1],"Rhizosphere_Manipulation"=data[,3],"Substrate_Type"=data[,4],"Microbial_Group"=data[,14],"d13"=data[,9])
   
   data_wide<-dcast(data_trim,Sample_Name+Rhizosphere_Manipulation+Substrate_Type ~ Microbial_Group, value.var="d13", fun.aggregate=mean, na.rm=TRUE)
+  
+  is.nan.data.frame <- function(x)
+    do.call(cbind, lapply(x, is.nan))
   data_wide[is.nan(data_wide)] <- NA
   
-  data_wide$BACTERIA<-mean(c(data_wide$bacteria,data_wide$`gram+bacteria`,data_wide$`gram-bacteria`),na.rm=TRUE)
-  data_wide$FUNGI<-mean(c(data_wide$AMF,data_wide$fungi),na.rm=TRUE)
-  data_wide$ALL_MICROBES<-mean(c(data_wide$AMF,data_wide$bacteria,data_wide$fungi,data_wide$general,data_wide$`gram-bacteria`,data_wide$`gram+bacteria`),na.rm=TRUE)
-  data_wide<-data_wide[,c(1,2,3,4,11,12,13)]
+      ### Take mean of narrow groups to yield FUNGI, BACTERIA, ALL_MICROBE groups
+      for (i in 1:nrow(data_wide)) {
+        data_wide$BACTERIA[i]<-mean(c(data_wide$bacteria[i],data_wide$`gram+bacteria`[i],data_wide$`gram-bacteria`[i]),na.rm=TRUE)
+        data_wide$FUNGI[i]<-mean(c(data_wide$AMF[i],data_wide$fungi[i]),na.rm=TRUE)
+        data_wide$ALL_MICROBES[i]<-mean(c(data_wide$AMF[i],data_wide$bacteria[i],data_wide$fungi[i],data_wide$general[i],data_wide$`gram-bacteria`[i],data_wide$`gram+bacteria`[i],data_wide$protozoa[i]),na.rm=TRUE)
+      }
+
+  data_wide<-data_wide[,c(1,2,3,4,11,12,13)] # Trim down to broader groups
   
   data<-melt(data_wide,value.name="d13",id.vars=c("Sample_Name","Rhizosphere_Manipulation","Substrate_Type"))
   names(data)[4]<-"Microbial_Group"
@@ -98,10 +107,11 @@ library(reshape2)
 
   ## Leaf PLFA figure
   leaf_d13_PLFA<-ggplot(leaf_PLFA,aes(x=Rhizosphere_Manipulation,y=d13,fill=Microbial_Group)) +
+    geom_hline(yintercept=-25,lwd=1.5,linetype="dashed") +
     geom_boxplot(lwd=1) +
-    scale_fill_brewer(palette = "Set1") +
     ylab(label=expression(bold(paste("\u03B4"^{bold("13")}, "C PLFA"," (\u2030)")))) +
     scale_x_discrete(labels=c("1"="-R-M","2"="-R+M","3"="+R+M")) +
+    scale_fill_viridis(discrete=TRUE,labels=c("AMF","Bacteria","Fungi","All Microbes")) +
     xlab(label="Rhizosphere Manipulation") +
     ggtitle("Leaf Substrate") +
     labs(fill="Microbial Group") +
@@ -112,19 +122,28 @@ library(reshape2)
           axis.title=element_text(size=14,face="bold"),
           plot.title=element_text(size=14,face="bold"),
           panel.border=element_rect(fill=NA,colour="black",size=1.5),
+          legend.text=element_text(colour="black",size=14),
+          legend.title=element_text(colour="black",size=14,face="bold"),
           panel.background=element_rect(fill=NA),
           legend.position="none")
   
+  ## Leaf PLFA ANOVA and Tukey's HSD
+  leaf_PLFA_mod<-lm(data=leaf_PLFA,d13~Rhizosphere_Manipulation*Microbial_Group)
+  Anova(leaf_PLFA_mod,type="II") # Rhizosphere manipulation significant (p < 0.001), microbial group and interaction between microbial group and rhizosphere are insignificant (p = 0.9520 and 0.9630 respectively)
+  TukeyHSD(aov(leaf_PLFA_mod))
+  
   ## Starch PLFA figure
   starch_d13_PLFA<-ggplot(starch_PLFA,aes(x=Rhizosphere_Manipulation,y=d13,fill=Microbial_Group)) +
+    geom_hline(yintercept=-25,lwd=1.5,linetype="dashed") +
     geom_boxplot(lwd=1) +
-    scale_fill_brewer(palette = "Set1") +
+    scale_fill_viridis(discrete=TRUE,labels=c("AMF","Bacteria","Fungi","All Microbes")) +
     ylab(label=expression(bold(paste("\u03B4"^{bold("13")}, "C PLFA"," (\u2030)")))) +
     scale_x_discrete(labels=c("1"="-R-M","2"="-R+M","3"="+R+M")) +
     xlab(label="Rhizosphere Manipulation") +
+    geom_hline(yintercept=-25,lwd=1.5,linetype="dashed") +
     ggtitle("Starch Substrate") +
     labs(fill="Microbial Group") +
-    #ylim(-50,275) +
+    ylim(-50,150) + # Excludes 1 outlier
     theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
           axis.text.y=element_text(colour="black",size=10),
           axis.text.x=element_text(colour="black",size=14),
