72e3e994d65eab4ef2af4d5bfef9bb075af5acea
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2021-12-10 14:46:34 -0500

    Fixed 13C PLFA figures to include differences by tree

--- Current_CostaRica_Graphs.R
+++ Current_CostaRica_Graphs.R
@@ -1,7 +1,8 @@
 #### Current Costa Rica Figures
 #### KER
 #### Created on: 07 December 2021
-
+#### Last modified:
+####          10 December 2021: Modified figures to include tree differences
 
 # Load packages -----------------------------------------------------------
 
@@ -11,6 +12,7 @@ library(gridExtra)
 library(reshape2)
 library(viridis)
 library(car)
+library(agricolae)
 
 # delta13C-CO2 ------------------------------------------------------------
 
@@ -24,48 +26,56 @@ library(car)
   leaf_CO2<-filter(dat,dat$Isotope_label=="L")
 
   ## Leaf d13 figure
-  leaf_d13<-ggplot(data=leaf_CO2,aes(x=Exclusion,y=d13_d9)) +
-    geom_boxplot(lwd=1) +
-    geom_point(alpha=0.7,size=5) +
-    labs(x=expression(bold("Rhizosphere Manipulation")),y=expression(bold(paste("\u03B4"^{bold("13")},"CO"[bold("2")]," (\u2030)"))),title = expression(bold("Leaf Substrate"))) +
-    annotate("text", x = 1, y = 100, label = "A", size=8, color="red") +
-    annotate("text", x = 2, y = 100, label = "B", size=8, color="red") +
-    annotate("text", x = 3, y = 100, label = "B", size=8, color="red") +
+  leaf_d13<-ggplot(data=leaf_CO2,aes(x=Exclusion,y=d13_d9,fill=Tree_species)) +
+    geom_boxplot(lwd=1.5,outlier.shape=NA,alpha=0.8) +
+    geom_dotplot(binaxis="y",stackdir="center",alpha=0.7,position=position_dodge(0.8)) +
+    labs(x=expression(bold("Rhizosphere Manipulation")),y=expression(bold(paste("\u03B4"^{bold("13")},"CO"[bold("2")]," (\u2030)"))),title = expression(bold("Leaf Substrate")),fill=expression(bold("Tree Species"))) +
     ylim(-20,100) +
     scale_x_discrete(labels=c("1"="-R-M","2"="-R+M","3"="+R+M")) +
+    scale_fill_viridis(discrete=TRUE,labels=c("G. meintha","P. macroloba")) +
     theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
         axis.text.y=element_text(colour="black",size=10),
         axis.text.x=element_text(colour="black",size=14),
         axis.title=element_text(size=14,face="bold"),
+        legend.text=element_text(size=12,face="italic"),
+        legend.title=element_text(size=14,face="bold"),
         panel.border=element_rect(fill=NA,colour="black",size=1.5),
-        panel.background=element_rect(fill=NA))
+        panel.background=element_rect(fill=NA),
+        legend.position="none")
 
   ## 13C CO2 leaf anova and TukeyHSD
-  leafMod<-lm(leaf_CO2$d13_d9~leaf_CO2$Exclusion) # super super significant p < 0.001
-  anova(leafMod) # Significant rhizosphere manipulation
-  TukeyHSD(aov(leafMod)) # -R-M is different from the other two exclusion types
-
+  leafMod<-lm(d13_d9~Exclusion*Tree_species,data=leaf_CO2) # super super significant p < 0.001
+  Anova(leafMod,type="II") # Significant rhizosphere manipulation, tree, and tree*rhizo interaction
+  TukeyHSD(aov(leafMod)) 
+  tx<-with(leaf_CO2, interaction(Exclusion, Tree_species))
+  amod<-aov(d13_d9 ~ tx, data=leaf_CO2)
+  HSD.test(amod, "tx", group=TRUE, console=TRUE)
+  
   ## Starch d13 figure
-  starch_d13<-ggplot(data=starch_CO2,aes(x=Exclusion,y=d13_d2)) +
-    geom_boxplot(lwd=1) +
-    geom_point(size=5, alpha=0.7) +
-    labs(x=expression(bold("Rhizosphere Manipulation")),y=expression(bold(paste("\u03B4"^{bold("13")},"CO"[bold("2")]," (\u2030)"))),title = expression(bold("Starch Substrate"))) +
-    annotate("text", x = 1, y = 260, label = "A", size=8, color="red") +
-    annotate("text", x = 2, y = 260, label = "B", size=8, color="red") +
-    annotate("text", x = 3, y = 260, label = "B", size=8, color="red") +
+  starch_d13<-ggplot(data=starch_CO2,aes(x=Exclusion,y=d13_d2,fill=Tree_species)) +
+    geom_boxplot(lwd=1.5,outlier.shape=NA,alpha=0.8) +
+    geom_dotplot(binaxis="y",stackdir="center",alpha=0.7,position=position_dodge(0.8)) +
+    labs(x=expression(bold("Rhizosphere Manipulation")),y=expression(bold(paste("\u03B4"^{bold("13")},"CO"[bold("2")]," (\u2030)"))),title = expression(bold("Starch Substrate")),fill=expression(bold("Tree Species"))) +
     ylim(-20,260) +
     scale_x_discrete(labels=c("1"="-R-M","2"="-R+M","3"="+R+M")) +
+    scale_fill_viridis(discrete=TRUE,labels=c("G. meintha","P. macroloba")) +
     theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
-        axis.text.y=element_text(colour="black",size=10),
-        axis.text.x=element_text(colour="black",size=14),
-        axis.title=element_text(size=14,face="bold"),
-        panel.border=element_rect(fill=NA,colour="black",size=1.5),
-        panel.background=element_rect(fill=NA))
+          axis.text.y=element_text(colour="black",size=10),
+          axis.text.x=element_text(colour="black",size=14),
+          axis.title=element_text(size=14,face="bold"),
+          legend.text=element_text(size=12,face="italic"),
+          legend.title=element_text(size=14,face="bold"),
+          panel.border=element_rect(fill=NA,colour="black",size=1.5),
+          panel.background=element_rect(fill=NA),
+          legend.position="none")
 
   ## 13C CO2 starch anova and TukeyHSD
-  starchMod<-lm(starch_CO2$d13_d2~starch_CO2$Exclusion)
-  anova(starchMod) # Only significant differences are exclusion
+  starchMod<-lm(d13_d2~Exclusion*Tree_species,data=starch_CO2) # super super significant p < 0.001
+  Anova(starchMod,type="II") # Significant rhizosphere manipulation
   TukeyHSD(aov(starchMod))
+  tx<-with(starch_CO2, interaction(Exclusion, Tree_species))
+  amod<-aov(d13_d2 ~ tx, data=starch_CO2)
+  HSD.test(amod, "tx", group=TRUE, console=TRUE)
 
   ## Panelled figure
   grid.arrange(leaf_d13,starch_d13,nrow=1)
@@ -81,9 +91,9 @@ library(car)
   data$umol_FAME<-data$nmol_FAME_per_g_soil/1000
   
   ## Simplify the microbial groupings
-  data_trim<-data.frame("Sample_Name"=data[,1],"Rhizosphere_Manipulation"=data[,3],"Substrate_Type"=data[,4],"Microbial_Group"=data[,14],"d13"=data[,9])
+  data_trim<-data.frame("Sample_Name"=data[,1],"Tree"=data[,2],"Rhizosphere_Manipulation"=data[,3],"Substrate_Type"=data[,4],"Microbial_Group"=data[,14],"d13"=data[,9])
   
-  data_wide<-dcast(data_trim,Sample_Name+Rhizosphere_Manipulation+Substrate_Type ~ Microbial_Group, value.var="d13", fun.aggregate=mean, na.rm=TRUE)
+  data_wide<-dcast(data_trim,Sample_Name+Tree+Rhizosphere_Manipulation+Substrate_Type ~ Microbial_Group, value.var="d13", fun.aggregate=mean, na.rm=TRUE)
   
   is.nan.data.frame <- function(x)
     do.call(cbind, lapply(x, is.nan))
@@ -96,10 +106,10 @@ library(car)
         data_wide$ALL_MICROBES[i]<-mean(c(data_wide$AMF[i],data_wide$bacteria[i],data_wide$fungi[i],data_wide$general[i],data_wide$`gram-bacteria`[i],data_wide$`gram+bacteria`[i],data_wide$protozoa[i]),na.rm=TRUE)
       }
 
-  data_wide<-data_wide[,c(1,2,3,4,11,12,13)] # Trim down to broader groups
+  data_wide<-data_wide[,c(1,2,3,4,5,12,13,14)] # Trim down to broader groups
   
-  data<-melt(data_wide,value.name="d13",id.vars=c("Sample_Name","Rhizosphere_Manipulation","Substrate_Type"))
-  names(data)[4]<-"Microbial_Group"
+  data<-melt(data_wide,value.name="d13",id.vars=c("Sample_Name","Rhizosphere_Manipulation","Tree","Substrate_Type"))
+  names(data)[5]<-"Microbial_Group"
 
   ## Separate leaf and starch datasets
   leaf_PLFA<-filter(data,Substrate_Type=="L")
@@ -115,25 +125,30 @@ library(car)
     xlab(label="Rhizosphere Manipulation") +
     ggtitle("Leaf Substrate") +
     labs(fill="Microbial Group") +
-    #ylim(-25,75) +
+    facet_wrap(~Tree,labeller=labeller(Tree=tree_labs)) +
     theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
           axis.text.y=element_text(colour="black",size=10),
           axis.text.x=element_text(colour="black",size=14),
           axis.title=element_text(size=14,face="bold"),
           plot.title=element_text(size=14,face="bold"),
           panel.border=element_rect(fill=NA,colour="black",size=1.5),
-          legend.text=element_text(colour="black",size=14),
-          legend.title=element_text(colour="black",size=14,face="bold"),
           panel.background=element_rect(fill=NA),
+          strip.text.x=element_text(size=14,face="bold.italic"),
           legend.position="none")
   
   ## Leaf PLFA ANOVA and Tukey's HSD
-  leaf_PLFA_mod<-lm(data=leaf_PLFA,d13~Rhizosphere_Manipulation*Microbial_Group)
-  Anova(leaf_PLFA_mod,type="II") # Rhizosphere manipulation significant (p < 0.001), microbial group and interaction between microbial group and rhizosphere are insignificant (p = 0.9520 and 0.9630 respectively)
+  leaf_PLFA_mod<-lm(data=leaf_PLFA,d13~Rhizosphere_Manipulation*Microbial_Group*Tree)
+  Anova(leaf_PLFA_mod,type="II") # Rhizosphere manipulation significant (p < 0.001), rhizosphere x tree significant
   TukeyHSD(aov(leaf_PLFA_mod))
+  tx<-with(leaf_PLFA, interaction(Rhizosphere_Manipulation,Microbial_Group,Tree))
+  amod<-aov(d13 ~ tx, data=leaf_PLFA)
+  HSD.test(amod, "tx", group=TRUE, console=TRUE)
   
   ## Starch PLFA figure
-  starch_d13_PLFA<-ggplot(starch_PLFA,aes(x=Rhizosphere_Manipulation,y=d13,fill=Microbial_Group)) +
+  tree_labs<-c("G. meintha", "P. macroloba")
+  names(tree_labs)<-c("G", "P")
+  
+  starch_d13_PLFA<-ggplot(data=starch_PLFA,aes(x=Rhizosphere_Manipulation,y=d13,fill=Microbial_Group)) +
     geom_hline(yintercept=-25,lwd=1.5,linetype="dashed") +
     geom_boxplot(lwd=1) +
     scale_fill_viridis(discrete=TRUE,labels=c("AMF","Bacteria","Fungi","All Microbes")) +
@@ -144,6 +159,7 @@ library(car)
     ggtitle("Starch Substrate") +
     labs(fill="Microbial Group") +
     ylim(-50,150) + # Excludes 1 outlier
+    facet_wrap(~Tree,labeller=labeller(Tree=tree_labs)) +
     theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
           axis.text.y=element_text(colour="black",size=10),
           axis.text.x=element_text(colour="black",size=14),
@@ -151,11 +167,11 @@ library(car)
           plot.title=element_text(size=14,face="bold"),
           panel.border=element_rect(fill=NA,colour="black",size=1.5),
           panel.background=element_rect(fill=NA),
+          strip.text.x=element_text(size=14,face="bold.italic"),
           legend.position="none")
   
   ## Panneled figure
   grid.arrange(leaf_d13_PLFA,starch_d13_PLFA,ncol=2)
-  
 
 # Respiration (non-labelled) ----------------------------------------------
 
@@ -193,4 +209,41 @@ library(car)
   TukeyHSD(aov(resp_mod)) # 3 is different from 1 and 2
   
 
+# Unlabeled PLFA ----------------------------------------------------------
+
+  ## Read in data
+  data<-read.csv("FINAL_PLFA_with_metadata.csv")
+  data<-data[,2:15] # Gets rid of ID column
+  data<-filter(data,Microbial_Group!="standard")
+  data$Rhizosphere_Manipulation<-as.factor(data$Rhizosphere_Manipulation)
+  data$umol_FAME<-data$nmol_FAME_per_g_soil/1000
+  
+  ## Simplify the microbial groupings
+  data_trim<-data.frame("Sample_Name"=data[,1],"Tree"=data[,2],"Rhizosphere_Manipulation"=data[,3],"Microbial_Group"=data[,14],"umol_FAME"=data[,15])
+  
+  data_wide<-dcast(data_trim,Sample_Name+Tree+Rhizosphere_Manipulation ~ Microbial_Group, value.var="umol_FAME", fun.aggregate=sum, na.rm=TRUE)
+  
+  is.nan.data.frame <- function(x)
+    do.call(cbind, lapply(x, is.nan))
+  data_wide[is.nan(data_wide)] <- NA
+  
+  ### Take mean of narrow groups to yield FUNGI, BACTERIA, ALL_MICROBE groups
+  for (i in 1:nrow(data_wide)) {
+    data_wide$BACTERIA[i]<-sum(c(data_wide$bacteria[i],data_wide$`gram+bacteria`[i],data_wide$`gram-bacteria`[i]))
+    data_wide$FUNGI[i]<-sum(c(data_wide$AMF[i],data_wide$fungi[i]))
+    data_wide$ALL_MICROBES[i]<-sum(c(data_wide$AMF[i],data_wide$bacteria[i],data_wide$fungi[i],data_wide$general[i],data_wide$`gram-bacteria`[i],data_wide$`gram+bacteria`[i],data_wide$protozoa[i]))
+  }
+  
+  data_wide<-data_wide[,c(1,2,3,4,11,12,13)] # Trim down to broader groups
+  
+  data<-melt(data_wide,value.name="umol_FAME",id.vars=c("Sample_Name","Tree","Rhizosphere_Manipulation"))
+  names(data)[4]<-"Microbial_Group"
+  
+  ## ANOVA for general PLFA by rhizosphere manipulation and tree type
+  PLFA_mod<-lm(data=data,umol_FAME~Rhizosphere_Manipulation*Microbial_Group+Tree)
+  Anova(PLFA_mod,type="II") # Rhizospehre manipulation not significant, microbial group significant, tree significant, rhizosphere*microbial group not significant
+  
+  TukeyHSD(aov(PLFA_mod))
+  HSD.test(y=aov(PLFA_mod),group=TRUE)
+  
   
